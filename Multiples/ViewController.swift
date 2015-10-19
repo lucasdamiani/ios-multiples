f8c46c294912f99a15a2ffd0861fea0d7cca98b5
Lucas Damiani <lucasdamiani@gmail.com>
2015-10-19 00:19:22 -0200

    Initial commit

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -8,18 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private let threshold = 50
+    private var multiple = 0
+    private var sum = 0 {
+        didSet {
+            addLabel.text = "\(oldValue) + \(multiple) = \(sum)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var multipleTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = nil
+        addDoneButton(textField)
     }
-
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func play() {
+        if canStartGame() {
+            multiple = Int(multipleTextField.text!)!
+            startGame()
+        }
+    }
+    
+    @IBAction func add() {
+        sum += multiple
+        
+        if isGameOver() {
+            resetGameData()
+            endGame()
+        }
+    }
+    
+    private func canStartGame() -> Bool {
+        return multipleTextField.hasText()
+    }
+    
+    private func startGame() {
+        toggleGameState()
+    }
+    
+    private func isGameOver() -> Bool {
+        return sum >= threshold
+    }
+    
+    private func resetGameData() {
+        multiple = 0
+        sum = 0
+    }
+    
+    private func endGame() {
+        addLabel.text = "Press Add to add!"
+        toggleGameState()
+    }
+    
+    private func toggleGameState() {
+        logoImageView.hidden = !logoImageView.hidden
+        multipleTextField.hidden = !multipleTextField.hidden
+        playButton.hidden = !playButton.hidden
+        addLabel.hidden = !addLabel.hidden
+        addButton.hidden = !addButton.hidden
+    }
+    
+    private func addDoneButton(textField: UITextField!) {
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace,
+            target: nil, action: nil)
+        let doneBarButton = UIBarButtonItem(barButtonSystemItem: .Done,
+            target: view, action: Selector("endEditing:"))
+        keyboardToolbar.items = [flexBarButton, doneBarButton]
+        textField.inputAccessoryView = keyboardToolbar
+    }
 }
 
